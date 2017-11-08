b5b0c32143108ac5c808aefd50e16ab272665ce0
markpijnenburg <markpijnenburg@hotmail.nl>
2017-11-08 21:06:45 +0100

    Fixed style and comments

--- Homework/Week2/graph.js
+++ Homework/Week2/graph.js
@@ -1,105 +1,115 @@
+// Name: Mark Pijnenburg
+// Data Processing - Minor Programmeren - Universiteit van Amsterdam
+// Student number: 11841117
 
+// Main function that gets called when window is loaded.
 function createGraph(rawData){
 
+  // Calculates position on canvas.
   function createTransform(domain, range){
     var domain_min = domain[0]
     var domain_max = domain[1]
     var range_min = range[0]
     var range_max = range[1]
 
-    // formulas to calculate the alpha and the beta
+    // Formulas to calculate the alpha and the beta.
     var alpha = (range_max - range_min) / (domain_max - domain_min)
     var beta = range_max - alpha * domain_max
 
-    // returns the function for the linear transformation (y= a * x + b)
+    // Returns the function for the linear transformation (y= a * x + b).
     return function(x){
       return alpha * x + beta;
     }
   }
-  // Determine maximum temperature from array
-  function getMaxTemp(data_array){
+  // Determine maximum temperature from array.
+  function getMaxTemp(dataArray){
     var max = 0;
-    for (var i = 0; i < data_array.length; i++){
-      if (data_array[i][1] > max){
-        max = data_array[i][1];
+    for (var i = 0; i < dataArray.length; i++){
+      if (dataArray[i][tempIndex] > max){
+        max = dataArray[i][tempIndex];
       }
     }
     return max;
 
-  // Determine minimum temperature from array
+  // Determine minimum temperature from array.
   }
-  function getMinTemp(data_array){
+  function getMinTemp(dataArray){
     var min = 0;
-    for (var i = 0; i < data_array.length; i++){
-      if (data_array[i][1] < min){
-        min = data_array[i][1];
+    for (var i = 0; i < dataArray.length; i++){
+      if (dataArray[i][tempIndex] < min){
+        min = dataArray[i][tempIndex];
       }
     }
     return min;
   }
 
-  // Draw empty graph
-  function initGraph(temp_scale){
+  // Draw empty graph.
+  function initGraph(tempScale){
     context.beginPath();
     context.lineWidth = 3;
 
-    context.moveTo(padding, padding); // y axis
+    // Y axis
+    context.moveTo(padding, padding);
     context.lineTo(padding,height - padding);
 
-    context.moveTo(padding,height - padding);// x axis
+    // X axis
+    context.moveTo(padding,height - padding);
     context.lineTo(width,height - padding);
-
-    // context.fillText(temp_scale[0], 20, 10);
   }
 
   // Parse raw data to array
   function parseRawData(rawData){
-    var data_array = rawData.split('\n');
-    data_array.splice(0,12);
-    data_array.splice(data_array.length - 1, 2);
+    var dataArray = rawData.split('\n');
 
-    for (var i = 0; i < data_array.length; i++) {
-        data_array[i] = data_array[i].split(',');
-        data_array[i].splice(0,1);
-        // console.log(data_array[i][0].slice(0,4));
-        var year = data_array[i][0].slice(0,4);
-        var month = data_array[i][0].slice(4,6);
-        var day = data_array[i][0].slice(6,8);
-        data_array[i][0] = new Date(`${year}-${month}-${day}`);
-        data_array[i][1] = Number(data_array[i][1]);
-        // console.log(data_array[i]);
+    // Remove metadata/headers from txt loaded.
+    dataArray.splice(0, txtHeader);
+
+    // Remove empty array at the end.
+    dataArray.splice(dataArray.length - 1, 2);
+
+    // Parse raw data into date objects / numbers.
+    for (var i = 0; i < dataArray.length; i++) {
+        dataArray[i] = dataArray[i].split(',');
+        dataArray[i].splice(0,1);
+
+        // Extract date info into variables.
+        var year = dataArray[i][0].slice(0,4);
+        var month = dataArray[i][0].slice(4,6);
+        var day = dataArray[i][0].slice(6,8);
+
+        dataArray[i][0] = new Date(`${year}-${month}-${day}`);
+        dataArray[i][1] = Number(dataArray[i][1]);
     }
-    // console.log(data_array);
-    return data_array;
+    return dataArray;
   }
 
-  // Add labels to y and x axis
-  function addLabels(temp_scale, month_list){
+  // Add labels to y and x axis.
+  function addLabels(tempScale, monthList){
     var labelLineLength = 10;
 
-    // y axis
-
-
-    for (var i = 0; i < 8; i++){
+    // Y axis labels.
+    for (var i = 0; i < numberOfYLabels; i++){
       context.moveTo(padding, padding * i + padding);
       context.lineTo(padding - labelLineLength, padding * i + padding);
-      context.fillText(temp_scale[i], 20, padding * i + padding);
+      context.fillText(tempScale[i], textYOffset, padding * i + padding);
     }
 
-    // x axis
-    for (var i = 0; i < 12; i++){
-      context.moveTo((width-padding)/12 * i + padding, height - padding);
-      context.lineTo((width-padding)/12 * i + padding, height - (padding- labelLineLength));
-      context.fillText(month_list[i], (width-padding)/12 * i + (padding + 30), height - (padding- labelLineLength * 1.5));
+    // X axis labels.
+    for (var i = 0; i < numberOfXLabels; i++){
+      context.moveTo((width-padding)/numberOfXLabels * i + padding, height - padding);
+      context.lineTo((width-padding)/numberOfXLabels * i + padding, height - (padding- labelLineLength));
+      context.fillText(monthList[i], (width-padding)/numberOfXLabels * i + (padding + textXOffset), height - (padding- labelLineLength * 1.5));
     }
 
+    // Add title to the graph.
     context.font="15px Arial";
-    context.fillText("Months of the year", (width-padding) / 2 - padding, 440)
+    context.fillText("Months of the year", (width-padding) / 2 - padding, graphTitleOffset)
     context.font="20px Georgia";
-    context.fillText("Average temperature in De Bilt (2016)", (width - padding) / 3, 30);
+    context.fillText("Average temperature in De Bilt (2016)", (width - padding) / 3, textXOffset);
     context.stroke();
   }
 
+  // Add text to X axis.
   function yAxisText(text){
     context.save();
     context.translate(100,300);
@@ -109,46 +119,62 @@ function createGraph(rawData){
     context.restore();
   }
 
-  function dateToIndex(data_array){
-    var startDate = data_array[0][0];
+  // Convert date object to absolute day index.
+  // https://stackoverflow.com/questions/8619879/javascript-calculate-the-day-of-the-year-1-366
+  function dateToIndex(dataArray){
+    var startDate = dataArray[dateIndex][dateIndex];
     var oneDay = 1000 * 60 * 60 * 24;
-    for (var i = 0; i < data_array.length; i++){
-        var currentDate = data_array[i][0];
+    for (var i = 0; i < dataArray.length; i++){
+        var currentDate = dataArray[i][dateIndex];
         var differenceDate = currentDate - startDate;
-        data_array[i][0] = (Math.floor(differenceDate / oneDay)) + 1;
+        dataArray[i][dateIndex] = (Math.floor(differenceDate / oneDay)) + dateOffset;
     }
   }
 
-  function drawLines(data_array){
-    for (var i = 0; i < data_array.length - 1; i++){
+  // Draw lines in the graph.
+  function drawLines(dataArray){
+    for (var i = 0; i < dataArray.length - dateOffset; i++){
         context.beginPath();
-        context.moveTo(xTransform(data_array[i][0]), yTransform(data_array[i][1]));
-        context.lineTo(xTransform(data_array[i + 1][0]), yTransform(data_array[i + 1][1]));
+        context.moveTo(xTransform(dataArray[i][dateIndex]), yTransform(dataArray[i][tempIndex]));
+        context.lineTo(xTransform(dataArray[i + dateOffset][dateIndex]), yTransform(dataArray[i + dateOffset][tempIndex]));
         context.stroke();
     }
-
   }
 
-  var temp_scale = ["300", "250", "200", "150", "100", "50", "0", "-50"];
-  var month_list = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Okt", "Nov", "Dec"];
+  // Declaration of commonly used variables.
+  var tempScale = ["300", "250", "200", "150", "100", "50", "0", "-50"];
+  var monthList = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Okt", "Nov", "Dec"];
+  var tempIndex = 1;
+  var dateIndex = 0;
+  var txtHeader = 12;
+  var numberOfYLabels = 8;
+  var textYOffset = 20;
+  var numberOfXLabels = 12;
+  var textXOffset = 30;
+  var graphTitleOffset = 440;
+  var dateOffset = 1;
+  var firstDay = 1;
   var canvas = document.getElementById("myCanvas");
   var context = canvas.getContext("2d");
   var width = canvas.width = 1000;
   var height = canvas.height = 450;
   var padding = 50;
-  var data_array = parseRawData(rawData);
-  var max_temp = getMaxTemp(data_array)
-  var min_temp = getMinTemp(data_array)
-  var xTransform = createTransform([1,366], [50,1000]);
-  var yTransform = createTransform([-50,300], [400,50]);
+  var dataArray = parseRawData(rawData);
+  var maxTemp = getMaxTemp(dataArray)
+  var minTemp = getMinTemp(dataArray)
+  var numberOfDays = dataArray.length;
+  var xTransform = createTransform([firstDay,numberOfDays], [padding,width]);
+  var yTransform = createTransform([-padding,tempScale[0]], [height-padding,padding]);
 
-  initGraph(temp_scale);
-  addLabels(temp_scale, month_list);
+  // Call the functions that actually draw graph.
+  initGraph(tempScale);
+  addLabels(tempScale, monthList);
   yAxisText("Temperatures in 0.1 Celcius");
-  dateToIndex(data_array);
-  drawLines(data_array);
+  dateToIndex(dataArray);
+  drawLines(dataArray);
 }
 
+// Execute script only if page completely loaded.
 window.onload = function(){
   var rawData = new XMLHttpRequest();
   rawData.onreadystatechange = function() {
